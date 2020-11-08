5bf47f79a7fab1c65ff537b1fe21cf3b1f43d26e
scientistkev <kpmcpherson@gmail.com>
2020-11-08 12:29:21 -0500

    adds main function constructor

--- preprocessing/preprocess_geolocation_md.py
+++ preprocessing/preprocess_geolocation_md.py
@@ -6,27 +6,40 @@ tab-separated file with delineated location data and accession number"""
 import pandas as pd  # For dataframe manipulation
 import re  # For splitting complex string combinations
 
-# Read in metadata from local file
+# Initialize main() function
 
-metadata_df = pd.read_csv("/genbank/metadata/2697049_metadata.tsv", sep="\t")
 
-# Save accession numbers as df to be merged later
-# Save geolocations as df with NaNs filled as 'None' (for regex purposes)
-# Save geolocations as list for manipulation
+def preprocess_geolocation_md():
 
-accession_df = pd.DataFrame(df["Accession"])
-geolocations_series = df["Geo_Location"].fillna("None")
-geolocations_list = geolocations_series.tolist()
+    # Read in metadata from local file
 
-# Make geolocation tuples list with regex separated rules on : and ,
+    metadata_df = pd.read_csv("/genbank/metadata/2697049_metadata.tsv", sep="\t")
 
-geolocations_tuples = [tuple(re.split(": |, ", i)) for i in geolocations_list]
+    # Save accession numbers as df to be merged later
+    # Save geolocations as df with NaNs filled as 'None' (for regex purposes)
+    # Save geolocations as list for manipulation
 
-# Save new DataFrame of geolocations with locX
-# delineated locations (i.e., loc1, loc2 etc.)
+    accession_df = pd.DataFrame(df["Accession"])
+    geolocations_series = df["Geo_Location"].fillna("None")
+    geolocations_list = geolocations_series.tolist()
 
-geolocations_df = pd.DataFrame(geolocations_tuples, columns=["loc1", "loc2", "loc3"])
+    # Make geolocation tuples list with regex separated rules on : and ,
 
-# Join with accession_df and export to tsv file named geolocations+accessions.tsv
+    geolocations_tuples = [tuple(re.split(": |, ", i)) for i in geolocations_list]
 
-accession_df.join(geolocations_df).to_csv("/data/geolocations+accession.tsv", sep="\t")
+    # Save new DataFrame of geolocations with locX
+    # delineated locations (i.e., loc1, loc2 etc.)
+
+    geolocations_df = pd.DataFrame(
+        geolocations_tuples, columns=["loc1", "loc2", "loc3"]
+    )
+
+    # Join with accession_df and export to tsv file named geolocations+accessions.tsv
+
+    accession_df.join(geolocations_df).to_csv(
+        "/data/geolocations+accession.tsv", sep="\t"
+    )
+
+
+if __name__ == "__main__":
+    preprocess_geolocation_md()
