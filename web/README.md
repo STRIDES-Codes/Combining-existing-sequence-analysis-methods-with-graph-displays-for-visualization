550c2397acc695587b8f0cc28d78f34257e943c0
Paul Rigor <rigorpr@amazon.com>
2020-11-08 07:38:51 -0800

    [NO TICKET] Minor clean up

--- web/README.md
+++ web/README.md
@@ -1,17 +1,38 @@
 web
----
+===
+
+Frameworks and tools
+--------------------
+
+
+    cytoscape.js: https://github.com/cytoscape/cytoscape.js
+    cytoscape.js tutorial demo: https://github.com/cytoscape/cytoscape.js-tutorial-demo 
+    networkx
 
 
 
+HOWTO: Start the web app (flask)
+--------------------------------
 
-* HOWTO: Start the web app (flask)
+
+Run the following command to start the flask server
 
 
     python flask_server.py
+    
+
+Navigate your browser to this url
+
+
+    http://localhost:5000/
 
 
 
-* HOWTO: Generate JSON data (sample) in `content/datasets/custom.js`
+HOWTO: Generate JSON data (sample) 
+-----------------------------------
+
+Sample data is by default stored in `content/datasets/custom.js`
+
 
 
     python utils/render_networkx_data.py
--- web/utils/render_networkx_data.py
+++ web/utils/render_networkx_data.py
@@ -1,6 +1,9 @@
-'''utils/render_networkx_data.py
+"""utils/render_networkx_data.py
 
-'''
+Initial version
+
+"""
+
 import os
 import matplotlib.pyplot as plt
 from networkx import nx
@@ -8,26 +11,58 @@ import networkx.readwrite.json_graph
 import networkx.readwrite.json_graph.cytoscape as cytoscape
 import json
 
-n = 10  # 10 nodes
-m = 20  # 20 edges
 
-G = nx.gnm_random_graph(n, m)
+def load_graph(ifilename=None):
+    """Load graph from file
 
-# some properties
-print("node degree clustering")
-for v in nx.nodes(G):
-    print(f"{v} {nx.degree(G, v)} {nx.clustering(G, v)}")
+    """
+    G = None
+    # Testing mode
 
-print()
-print("the adjacency list")
-for line in nx.generate_adjlist(G):
-    print(line)
+    if ifilename is None:
+        n = 10  # 10 nodes
+        m = 20  # 20 edges
 
-# Export data
-n_json = cytoscape.cytoscape_data(G)
-utils_dir = os.path.dirname(os.path.abspath(__file__))
-utils_dir = os.path.dirname(os.path.abspath(__file__))
+        G = nx.gnm_random_graph(n, m)
 
-with open('%s/../content/datasets/custom.json' % utils_dir,'w') as f:
-    f.write(json.dumps(n_json))
+        # some properties
+        print("node degree clustering")
+        for v in nx.nodes(G):
+            print(f"{v} {nx.degree(G, v)} {nx.clustering(G, v)}")
+
+        print()
+        print("the adjacency list")
+        for line in nx.generate_adjlist(G):
+            print(line)
+    else:
+        pass
+
+
+def render_json(G):
+    """Render json to content/datasets/custom.json
+
+    TODO: Make filename generic
+
+    """
+    # Export data
+    n_json = cytoscape.cytoscape_data(G)
+    utils_dir = os.path.dirname(os.path.abspath(__file__))
+    utils_dir = os.path.dirname(os.path.abspath(__file__))
+    ofilename = '%s/../content/datasets/custom.json' % utils_dir
+
+    with open(ofilename, 'w') as f:
+        f.write(json.dumps(n_json))
+
+
+def main():
+    """Main entry
+
+    """
+    # TODO: Handle CLI params
+    the_graph = load_graph()
+    render_json(the_graph)
+
+
+if __name__ == "__main__":
+    main()
 
